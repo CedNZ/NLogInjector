64f46c2572289c93d1f0c8a51b6e317612a29848
Ruskin Dantra <ruskin.dantra@promapp.com>
2017-04-05 15:15:01 +1200

    Some more unit tests

--- NLogInjector.UnitTests/nlog_ctor_injection.cs
+++ NLogInjector.UnitTests/nlog_ctor_injection.cs
@@ -1,4 +1,5 @@
-﻿using Autofac;
+﻿using System.Linq;
+using Autofac;
 using FluentAssertions;
 using NLog;
 using NUnit.Framework;
@@ -20,10 +21,29 @@ namespace NLogInjector.UnitTests
 			public bool IsLoggerNull => _logger == null;
 		}
 
+	    private class ctor_injection_test_class_two_params
+	    {
+	        public another_param AnotherParam { get; }
+	        private readonly ILogger _logger;
+
+	        public ctor_injection_test_class_two_params(ILogger logger, another_param anotherParam)
+	        {
+	            AnotherParam = anotherParam;
+	            _logger = logger;
+	        }
+
+	        public bool IsLoggerNull => _logger == null;
+	    }
+
+	    private class another_param
+	    {
+
+	    }
+
 		[Test]
 		public void logger_property_should_be_injected_if_used_via_autofac()
 		{
-			var containerBuilder = new ContainerBuilder();
+            var containerBuilder = new ContainerBuilder();
 			containerBuilder.RegisterModule<NLogModule>();
 			containerBuilder.RegisterType<ctor_injection_test_class>().SingleInstance();
 
@@ -35,5 +55,23 @@ namespace NLogInjector.UnitTests
 				tempClass.IsLoggerNull.Should().BeFalse();
 			}
 		}
+
+	    [Test]
+	    public void logger_property_should_be_injected_along_with_another_ctor_param_if_used_via_autofac()
+	    {
+	        var containerBuilder = new ContainerBuilder();
+	        containerBuilder.RegisterModule<NLogModule>();
+	        containerBuilder.RegisterType<another_param>().SingleInstance();
+	        containerBuilder.RegisterType<ctor_injection_test_class_two_params>().SingleInstance();
+
+	        using (var container = containerBuilder.Build())
+	        {
+	            var tempClass = container.Resolve<ctor_injection_test_class_two_params>();
+	            Assume.That(tempClass, Is.Not.Null);
+
+	            tempClass.IsLoggerNull.Should().BeFalse();
+	            tempClass.AnotherParam.Should().NotBeNull();
+	        }
+	    }
 	}
 }
